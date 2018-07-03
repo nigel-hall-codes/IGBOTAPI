cb8d12b6fa87eba51b23bbc51c7fd68e09779d48
hallshit <nigel672@gmail.com>
2018-07-03 15:29:54 -0700

    added auth

--- API/wmigbot.py
+++ API/wmigbot.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
         schedule.every().day.at("16:00").do(bot.post_new_menu_items)
         schedule.every().day.at("16:00").do(bot.post_daily_deal)
         schedule.every(6).hours.do(bot.post_meme)
-        schedule.every().day.at("23:28").do(bot.post_meme)
+        schedule.every().day.at("23:31").do(bot.post_meme)
 
         while True:
             schedule.run_pending()
